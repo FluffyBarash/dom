53d788e1a2e0e772436a37606453a5fa4f5691fe
FluffyBarash <paramonov.3022@gmail.com>
2024-04-22 22:31:56 +0300

    main2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div className="main-wrapper">
+        <Header />
+      </div>
+      <div className="section">
+        <Main />
+      </div>
+    </>
   );
 }
 
