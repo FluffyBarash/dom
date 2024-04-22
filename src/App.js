9406ce585b994d5cad13bd4378506beae530b2e3
FluffyBarash <paramonov.3022@gmail.com>
2024-04-22 22:41:12 +0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Sidebar_ from "./components/Sidebar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </div>
       <div className="section">
         <Main />
+        <Sidebar_ />
       </div>
     </>
   );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/Sidebar_.css";
+
+class Sidebar_ extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="sidebar">Sidebar</div>;
+  }
+}
+
+export default Sidebar_;
